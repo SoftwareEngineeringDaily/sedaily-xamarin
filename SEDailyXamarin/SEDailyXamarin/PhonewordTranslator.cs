9d9631116870e4f30eb1ad1d42f0b1d8fe540b97
Nelly Cheboi <ncheboi@nwvl.com>
2017-10-10 23:25:01 -0500

    started on Xamarin-Android tutorial

---
+++ SEDailyXamarin/SEDailyXamarin/PhonewordTranslator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace SEDailyXamarin
+{
+    public static class PhonewordTranslator
+    {
+        public static string ToNumber(string raw)
+        {
+            if (string.IsNullOrWhiteSpace(raw))
+                return "";
+            else
+                raw = raw.ToUpperInvariant();
+
+            StringBuilder newNumber = new StringBuilder();
+            foreach (char c in raw)
+            {
+                if (" -0123456789".Contains(c))
+                {
+                    newNumber.Append(c);
+                }
+                else
+                {
+                    var result = TranslateToNumber(c);
+                    if (result != null)
+                        newNumber.Append(result);
+                }
+                // otherwise we've skipped a non-numeric char
+            }
+            return newNumber.ToString();
+        }
+        static bool Contains(this string keyString, char c)
+        {
+            return keyString.IndexOf(c) >= 0;
+        }
+        static int? TranslateToNumber(char c)
+        {
+            if ("ABC".Contains(c))
+                return 2;
+            else if ("DEF".Contains(c))
+                return 3;
+            else if ("GHI".Contains(c))
+                return 4;
+            else if ("JKL".Contains(c))
+                return 5;
+            else if ("MNO".Contains(c))
+                return 6;
+            else if ("PQRS".Contains(c))
+                return 7;
+            else if ("TUV".Contains(c))
+                return 8;
+            else if ("WXYZ".Contains(c))
+                return 9;
+            return null;
+        }
+    }
+}
